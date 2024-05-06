e766bc0c49eec95d0653c13f62f3665d5f8ad80b
Philip Ashton <philligan1@gmail.com>
2024-05-06 14:17:01 -0500

    input connections

--- client.js
+++ client.js
@@ -5,4 +5,17 @@ function submitForm(event) {
     // Stop the page from refreshing
     event.preventDefault();
 
+
+    let firstNameVal = document.querySelector('#firstName-input').value;
+    console.log(firstNameVal);
+    let lastNameVal = document.querySelector('#lastName-input').value;
+    console.log(lastNameVal);
+    let IDVal = document.querySelector('#ID-input').value;
+    console.log(IDVal);
+    let titleVal = document.querySelector('#title-input').value;
+    console.log(titleVal);
+    let salaryVal = document.querySelector('#salary-input').value;
+    console.log(salaryVal);
+
+    let employeeTable = document.querySelector('#employeeData');
 }
